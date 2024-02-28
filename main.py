0a0b3da2da45e28391965b4991044016f859c3b7
retr0-init <146389702+retr0-init@users.noreply.github.com>
2024-02-28 00:06:23 +0000

    bugfix

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class Retr0initDiscordUtilities(interactions.Extension):
     Check the permission to run the privileged command
     The ROLE_ID needs to be set with the elevate command
     '''
-    async def my_check(self, ctx: interactions.BaseContext):
+    async def my_check(ctx: interactions.BaseContext):
         res: bool = await interactions.is_owner()(ctx)
         r: bool = any(map(ctx.author.has_role, elevation_roles)) if len(self.elevation_roles) > 0 else False
         u: bool = any(map(ctx.author.id.__eq__, elevation_members)) if len(elevation_members) > 0 else False
